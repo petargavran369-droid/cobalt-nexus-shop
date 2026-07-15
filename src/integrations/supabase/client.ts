7359330c37452f01326bdae7b29a5521048420f7
petargavran369-droid <petargavran369@gmail.com>
2026-07-15 11:39:59 +0200

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from '../types';
 
-const SUPABASE_URL = "https://supabase.co";
+const SUPABASE_URL = "https://supabase.net";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_a116bdfb2b24b20536c4b223c72b88b0dcafb629";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
